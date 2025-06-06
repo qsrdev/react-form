699e7b1cbe5d2ff621455efcdc49e35b9bf1a0e9
qsrdev <matteo.albertini747@gmail.com>
2025-06-06 15:40:19 +0200

    first commit

---
+++ data/posts.js
@@ -0,0 +1,27 @@
+const blogPosts = [
+  {
+    id: 1717668000000,
+    titolo: "Benvenuti nel nostro blog",
+    contenuto: "Scopri le ultime novità, consigli e storie direttamente dalla nostra redazione.",
+  },
+  {
+    id: 1717668005000,
+    titolo: "Come coltivare un orto biologico",
+    contenuto: "Una guida pratica per iniziare a coltivare ortaggi sani e naturali nel proprio giardino.",
+  },
+  {
+    id: 1717668010000,
+    titolo: "5 ricette con il miele da provare subito",
+    contenuto: "Dalle tisane ai dolci: ecco alcune idee semplici e gustose per usare il miele in cucina.",
+  },
+  {
+    id: 1717668015000,
+    titolo: "La vita in campagna: un ritorno alla natura",
+    contenuto: "Un racconto quotidiano fatto di animali, stagioni e silenzio rigenerante.",
+  },
+  {
+    id: 1717668020000,
+    titolo: "Perché scegliere prodotti a km zero",
+    contenuto: "Un approfondimento sui vantaggi ambientali ed economici del consumo locale.",
+  },
+];
