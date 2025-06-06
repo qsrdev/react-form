ecf4a3abf3a632d839f6b744714e33e5e28de7d0
qsrdev <matteo.albertini747@gmail.com>
2025-06-06 17:32:51 +0200

    feat: going forward

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import posts from "../data/posts.js";
 import "./App.css";
 
 function App() {
+  const [newPost, setNewPost] = useState("");
+  const [postList, setPostList] = useState(posts);
+
+  const handleNewPost = (event) => {
+    event.preventDefault();
+    posts.push(newPost);
+    setPostList([newPost, ...postList]);
+    console.log(postList);
+  };
+
   return (
     <>
       <div className="container">
         <div className="w-50 mx-auto">
-          <h2>I miei post</h2>
+          <h2 className="text-center">I miei post</h2>
+
+          <form onSubmit={handleNewPost} className="d-flex my-5">
+            <input value={newPost} onChange={(event) => setNewPost(event.target.value)} autoComplete="off" className="form-control" type="text" aria-label="Aggiungi un nuovo articolo" />
+            <button className="btn btn-success mx-2">+</button>
+          </form>
+
           <ul className="list-group">
-            {posts.map((curPost, index) => (
+            {postList.map((curPost, index) => (
               <li key={index} className="list-group-item">
                 {curPost.titolo}
               </li>
